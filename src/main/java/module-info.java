63aad51416c7555cc6121e7b052e33363d21e5cf
Everett Hinckley <hincea@farmingdale.edu>
2024-04-02 01:57:24 -0400

    Need to change deleteFuture method to actually change data in ChoiceBoxes(just errors right now), being able to add classes for students(requires Student and Course classes), and connections to homepages.(possibly change look as well)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 module com.example.schoolregistrar {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires org.kordamp.bootstrapfx.core;
-=======
-module com.example.login {
-    requires javafx.controls;
-    requires javafx.fxml;
->>>>>>> 7819f9a (Needs to be able to add data from Firebase into ChoiceBoxes and needs connections to dashboards)
+
     requires firebase.admin;
     requires com.google.auth;
     requires com.google.auth.oauth2;
@@ -17,12 +10,6 @@ module com.example.login {
     requires google.cloud.core;
     requires com.google.api.apicommon;
 
-<<<<<<< HEAD
     opens com.example.schoolregistrar to javafx.fxml;
     exports com.example.schoolregistrar;
-=======
-
-    opens com.example.login to javafx.fxml;
-    exports com.example.login;
->>>>>>> 7819f9a (Needs to be able to add data from Firebase into ChoiceBoxes and needs connections to dashboards)
 }
